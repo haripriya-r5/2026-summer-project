c8081720e0cff8203945e206d531a3a197715556
Haripriya <haripriya.r5@outlook.com>
2026-08-28 17:24:51 -0400

    completed day3 week1

---
+++ week-1/day3-week1/file_handling.py
@@ -0,0 +1,40 @@
+## read file
+"""
+# read entire file
+with open("practice_file-handling.txt", "r") as file:
+    content = file.read()
+print(content)
+
+# read line by line
+with open("practice_file-handling.txt", "r") as file:
+    for line in file:
+        print(line.strip())
+
+# read into a list
+with open("practice_file-handling.txt", "r") as file:
+    lines = file.readlines()
+    print(lines)
+"""
+
+## extract
+
+# extract words
+with open("practice_file-handling.txt") as file:
+    words = file.read().split()
+print(words)
+
+# extract lines
+with open("practice_file-handling.txt") as file:
+    #overcomplicated version of read file line by line
+    new_lines = []
+    for line in file:
+        new_lines = line.strip()
+        print(new_lines)
+
+
+# extract characters
+with open("practice_file-handling.txt") as file:
+    characters = []
+    for line in file:
+        characters += line.strip()
+print(characters)
